ff68abaa0e558d6eb3ba7294319caae5da4374c2
dianaostik <dianastud12@gmail.com>
2022-10-26 16:04:47 +0300

    com8

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ console.log("------ 1 uzduotis ---------");
 
 let word = "labas";
 for (let i = 1; i < 11; i++) {
-    console.log( i + " " + word );
+    console.log(i + " " + word);
 }
 
 //2 uzd
 console.log("------ 2 uzduotis ---------");
 
 for (let i = 0; i < 10; i++) {
-    console.log( i );
+    console.log(i);
 }
 
 //3 uzd
@@ -30,14 +30,14 @@ for (let i = 0; i < 10; i++) {
 //5 uzd
 console.log("------ 5 uzduotis ---------");
 
-for (let i = words.length -1; i >= 0; i--) {
+for (let i = words.length - 1; i >= 0; i--) {
     console.log(words[i]);
 }
 
 //6 uzd
 console.log("------ 6 uzduotis ---------");
 
-for (let i = 10; i <= 50; i+=2) {
+for (let i = 10; i <= 50; i += 2) {
     console.log(i);
 }
 
@@ -45,12 +45,33 @@ for (let i = 10; i <= 50; i+=2) {
 console.log("------ 7 uzduotis ---------");
 
 let tuscia = "";
-for (let i = 10; i <= 50; i+=2) {
-    if (i === 10 || i === 20 || i === 30 || i === 40 || i === 50) 
-    { continue;}   
-    console.log(tuscia + i + "  ");  
+for (let i = 10; i <= 50; i += 2) {
+    if (i === 10 || i === 20 || i === 30 || i === 40 || i === 50) { continue; }
+    console.log(tuscia + i + "  ");
 }
 
 //8 uzd
 console.log("------ 8 uzduotis ---------");
+
+sum = 0;
+for (let i = 0; i <= 20; i++) {
+    if ( i%2 == 0 ) { continue; }
+    sum++;
+}
+console.log(sum + "  "); 
+
+// 7/3 2.3333
+// 7%3 1
+// 7 -3 =4; 4-3 = 1;
+
+// 12%5 = 2;
+// 12 - 5 = 7; 7-5=2; 
+
+// let num = 6;
+
+// num % 2 == 0;
+// num % 2 != 0;
+
+//9 uzd
+console.log("------ 9 uzduotis ---------");
 
