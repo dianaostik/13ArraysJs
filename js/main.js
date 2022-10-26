6a61c17d9d22a7755f7f880a56efb9542a4a4758
dianaostik <dianastud12@gmail.com>
2022-10-26 14:50:31 +0300

    com

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+//1 uzd
+
+console.log("------ 1 uzduotis ---------");
+
+let word = "labas";
+for (let i = 1; i < 11; i++) {
+    console.log( i + " " + word );
+}
+
+//2 uzd
+
+console.log("------ 2 uzduotis ---------");
+
+for (let i = 0; i < 10; i++) {
+    console.log( i );
+}
+
+//3 uzd
+
+console.log("------ 3 uzduotis ---------");
