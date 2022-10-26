edb9f1429756c1fef69cd6cfd6573f833596473b
dianaostik <dianastud12@gmail.com>
2022-10-26 15:17:53 +0300

    com5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 //1 uzd
-
 console.log("------ 1 uzduotis ---------");
 
 let word = "labas";
@@ -9,7 +8,6 @@ for (let i = 1; i < 11; i++) {
 }
 
 //2 uzd
-
 console.log("------ 2 uzduotis ---------");
 
 for (let i = 0; i < 10; i++) {
@@ -17,5 +15,25 @@ for (let i = 0; i < 10; i++) {
 }
 
 //3 uzd
-
 console.log("------ 3 uzduotis ---------");
+
+let words = ["Astras", "Rožė", "Alyva", "Alavijas", "Akacija", "Aguona", "Dilgėlė", "Dobilas", "Kalendra", "Magnolija"];
+console.log(words);
+
+//4 uzd
+console.log("------ 4 uzduotis ---------");
+
+for (let i = 0; i < 10; i++) {
+    console.log(words[i]);
+}
+
+//5 uzd
+console.log("------ 5 uzduotis ---------");
+
+for (let i = words.length -1; i >= 0; i--) {
+    console.log(words[i]);
+}
+
+//6 uzd
+console.log("------ 6 uzduotis ---------");
+
